2b51b89f250ff907909dfa9bfe5444de013cc1ba
Zack Yang <zack9433@gmail.com>
2016-08-26 17:28:53 +0800

    fix(dependency): upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 var NODE_ENV = process.env.NODE_ENV;
 var nodeRoot = path.join(__dirname, 'node_modules');
 var appRoot = path.join(__dirname, 'app');
@@ -32,6 +33,7 @@ var config = {
     noParse: []
   },
   plugins: [
+    new ProgressBarPlugin(),
     new webpack.DefinePlugin({
       __TEST__: 'test' === NODE_ENV,
       __DEV__: 'development' === NODE_ENV,
