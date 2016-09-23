2a1d1757796b0865d1b2a9a5e3fae1472f73ec29
Zack Yang <zack9433@gmail.com>
2016-09-23 15:02:30 +0800

    chore(build): modify for webpack 2 breaking changes

--- webpack.build.js
+++ webpack.build.js
@@ -1,7 +1,5 @@
-'use strict';
-
-var webpack = require('webpack');
-var config = require('./webpack.config.js');
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
 
 config.devtool = 'source-map';
 config.entry = {
@@ -65,7 +63,8 @@ config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
     compress: {
       screw_ie8: true,
-      warnings: false
+      warnings: false,
+      dead_code: true
     }
   })
 );
